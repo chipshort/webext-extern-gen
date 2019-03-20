083539a430d06caa92c9cc15836cff245a199e64
Christoph Otter <christophotter@gmail.com>
2019-03-20 19:49:32 +0100

    Fix test extension

--- test/src/test/Main.hx
+++ test/src/test/Main.hx
@@ -1,12 +1,23 @@
 package test;
 
-import js.browser.Browser;
+import js.Browser.*;
 
 class Main
 {
     public static function main() : Void
     {
-        Browser.bookmarks._import(function () {});
-        // Browser.search.search({ tabId: 0, query: "test", engine: null});
+        #if chrome
+        chrome.History.search({
+            text: "test"
+        }, function (items) {
+            document.getElementById("data").innerText = Std.string(items);
+        });
+        #else
+        browser.History.search({
+            text: "test"
+        }).then(function (items : Array<browser.history.HistoryItem>) {
+            document.getElementById("data").innerText = Std.string(items);
+        });
+        #end
     }
 }
