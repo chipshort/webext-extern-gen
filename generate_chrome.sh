7c6f661d659062d5bc864b38a956946b6b6c8389
Christoph Otter <christophotter@gmail.com>
2019-03-24 02:50:24 +0100

    Add shell scripts for easy usage

---
+++ generate_chrome.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if ! [ -x "$(command -v python2)" ]; then
+  echo 'Error: Python2 is not installed.' >&2
+  exit 1
+fi
+if ! [ -x "$(command -v haxe)" ]; then
+  echo 'Error: Haxe is not installed.' >&2
+  exit 1
+fi
+
+set -e
+echo "Installing Haxelib dependencies..."
+haxelib install build.hxml --quiet
+echo "----------"
+
+#Convert chrome idl schemas
+python2 chromium_idl_converter/haxe_gen.py
+echo "----------"
+
+#Generate Haxe externs
+haxe build.hxml -D chrome
